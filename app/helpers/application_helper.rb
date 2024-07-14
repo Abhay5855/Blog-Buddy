ff8125597beb3dc36210404e4d71e879585bd98c
abhay <ap391929@gmail.com>
2024-07-14 21:21:40 +0530

    feat:add tailwind-css to the rails

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+
+  def user_initials(email)
+    name_part = email.split('@').first
+    initials = name_part.split('.').map { |part| part[0] }.join.upcase
+    initials
+  end
 end
