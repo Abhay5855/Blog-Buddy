8425389cbd2a07796202d8167d2f2a45a9e00842
abhay <ap391929@gmail.com>
2024-07-18 23:49:03 +0530

    feat:add comment section, draft and publish state, delete posts, and user profile

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,6 +7,11 @@ class Post < ApplicationRecord
 
   has_many :comments
 
+  enum status: { draft: 0, published: 1 }
+
+  validates :status, presence: true
+
+
   validates :title,  length: { maximum: 100,
     too_long: "%{count} characters is the maximum allowed", minimum: 5, too_short: "Title should be %{count} characters at least" }, presence: true
 
@@ -15,7 +20,7 @@ class Post < ApplicationRecord
   validates :content, presence: true
 
   validate  :image_format
-  
+
     private
 
   def image_format
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_16_143118) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_18_092042) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
@@ -69,6 +69,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_16_143118) do
     t.datetime "updated_at", null: false
     t.boolean "publish"
     t.boolean "draft"
+    t.integer "status"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
